cd6ad9cc3ca7e5ad0ebf38fa3b8d45673e45d7c8
Nihat Halici <nihat.halici@gmail.com>
2021-07-27 22:06:24 +0200

    Add files via upload

---
+++ C01-Exercises-with-numbers/Question-2.py
@@ -0,0 +1,13 @@
+"""
+Calculate the average of the following sequences of numbers:
+(2, 4), (4, 8, 9), (12,14/6, 15)
+
+"""
+
+x = [2, 4]
+y = [4, 8, 9]
+z = [12, 14 / 6, 15]
+
+print("Average of list x is:", sum(x) / len(x))
+print("Average of list y is:", sum(y) / len(y))
+print("Average of list z is:", sum(z) / len(z))
---
+++ C01-Exercises-with-numbers/Question-3.py
@@ -0,0 +1,12 @@
+"""
+The volume of a sphere is given by 4/3πr.
+Calculate the volume of a sphere of radius 5.
+
+Suggestion:
+Create a variable named "pi" with the value of 3.1415
+"""
+
+pi = 3.1415
+my_rad = 5
+my_vol = (4 / 3 * pi * my_rad)
+print(my_vol)
